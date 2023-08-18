b8c447a0711bdd95f8df5de99a7ccaab0d925436
morsalinamunmun <morsalinamunmun@gmail.com>
2023-08-18 13:17:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function calculateTriangleArea(){
+    //get triangle base value
+    const triangleBase = document.getElementById('triangle-base');
+    const base = parseFloat(triangleBase.value);
+    triangleBase.value = '';
+    //get triangle height value
+    const triangleHeight = document.getElementById('triangle-height');
+    const height = parseFloat(triangleBase.value);
+    const area = 0.5 * base * height;
+    //show triangle area
+    const areaValue = document.getElementById('triangle-area');
+    areaValue.innerText = area;
+    triangleHeight.value = '';
+    
+
+}
+
+function calculateRectangleArea(){
+    //get rectangle width value
+    const rectangleWidth = document.getElementById('rectangle-width');
+    rectangleWidth.value = '';
+    const width = parseFloat(rectangleWidth.value);
+    const rectangleLength = document.getElementById('rectangle-length');
+    const length = parseFloat(rectangleLength.value);
+    const area = width * length;
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
+}
